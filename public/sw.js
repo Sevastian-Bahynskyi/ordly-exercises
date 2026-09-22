4464a7e7d414f1189cf853363c9dbc268ba5bce1
Sevastian Bahynskyi <114170185+Sevastian-Bahynskyi@users.noreply.github.com>
2026-09-22 09:23:44 +0200

    Add PWA service worker

---
+++ public/sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'ordly-practice-v1'
+const SHELL = [
+  '/ordly-exercises/',
+  '/ordly-exercises/manifest.webmanifest',
+  '/ordly-exercises/icons/icon-180.png',
+  '/ordly-exercises/icons/icon-192.png',
+  '/ordly-exercises/icons/icon-512.png',
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.addAll(SHELL))
+      .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request
+  if (request.method !== 'GET' || new URL(request.url).origin !== self.location.origin) return
+
+  event.respondWith(
+    fetch(request)
+      .then((response) => {
+        const copy = response.clone()
+        caches.open(CACHE).then((cache) => cache.put(request, copy))
+        return response
+      })
+      .catch(() => caches.match(request).then((cached) => cached || caches.match('/ordly-exercises/')))
+  )
+})
